202d1bc5739ea5d1e2f4157c4782069cf9d1d39a
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2022-09-23 22:58:49 -0300

    Autenticacao com Google

--- src/Screens/Auth.js
+++ src/Screens/Auth.js
@@ -2,10 +2,24 @@ import React from 'react'
 import { Form, Button, Container } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
 import axiosInstance from '../myaxios';
+import { GoogleLogin } from 'react-google-login';
+
+
+
 
 const Auth = () => {
   const [form, setform] = React.useState({email: "", password: ""})
   const dispatch = useDispatch();
+
+  const responseGoogle = async (response) => {
+    const tokenId = response.tokenId;
+    const res = await axiosInstance.post("/auth/loginWithGoogle", { tokenId })
+    const data = await res.data;
+    const token = data.token;
+    localStorage.setItem("token", token);
+    dispatch({type: "LOGIN"});
+  }
+  
 
   const handleChange = e => {
     e.preventDefault();
@@ -36,6 +50,13 @@ const Auth = () => {
             <Button onClick={signIn} variant="primary" type="submit" >
                 Submit
             </Button>
+            <GoogleLogin
+    clientId="277380091468-1pe2je91eas7almtof0bf0bfhmehbvgi.apps.googleusercontent.com"
+    buttonText="Login With Google"
+    onSuccess={responseGoogle}
+    onFailure={responseGoogle}
+    cookiePolicy={'single_host_origin'}
+  />
         </Form>
         </Container>
 
