8bc44ea54098fa8de65b1449f1cd223118ad1d09
Fillipe de Souza Silva <fssilva@fiecalunos.lan>
2022-10-14 22:04:04 -0300

    Commit before deploying on azure

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,1 @@
-export const BACKEND_URL = "http://localhost:38000";
+export const BACKEND_URL = process.env.BACKEND_URL || "http://localhost:38000";
--- src/index.js
+++ src/index.js
@@ -14,20 +14,30 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 const store = createStore(reducer);
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <GoogleOAuthProvider clientId="609225504796-b42ba3tdkbiq6k76rlja6nsiec8bm391.apps.googleusercontent.com">
-  <Provider store={store}>
-  <HashRouter>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </HashRouter>
-  </Provider>
-  </GoogleOAuthProvider>
-);
+const renderReactDom = () => {
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+  root.render(
+    <GoogleOAuthProvider clientId="609225504796-b42ba3tdkbiq6k76rlja6nsiec8bm391.apps.googleusercontent.com">
+    <Provider store={store}>
+    <HashRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+    </HashRouter>
+    </Provider>
+    </GoogleOAuthProvider>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
+if(window.cordova){
+  document.addEventListener('deviceready', () => {
+    renderReactDom();
+  }, false);
+} else {
+    renderReactDom();
+}
